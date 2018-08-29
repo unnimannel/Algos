ae2918d0e817f500348bebafc421cc3655dc8f8d
voylite <unni@voylite.com>
2018-08-29 15:03:08 +0530

    Count using Efficient

--- ArrayElmCountFreq.py
+++ ArrayElmCountFreq.py
@@ -5,13 +5,18 @@ class CountFreqCls:
 		self.input = [3,4,4,5,7,4,4]
 
 	def start(self):
+		# print('input : ',self.input)
+		# print('count using naive starts')
+		# self.countFreqNaive()
+		# print('count using naive ends')
+		# print('input : ',self.input)
+		# print('count using normal starts')
+		# self.countFreq()
+		# print('count using normal ends')
 		print('input : ',self.input)
-		print('count using naive starts')
-		self.countFreqNaive()
-		print('count using naive ends')
-		print('count using normal starts')
-		self.countFreq()
-		print('count using normal ends')
+		print('count using Efficient starts')
+		self.countFreqEfficient()
+		print('count using Efficient ends')
 
 	def countFreqNaive(self):
 		n = len(self.input)
@@ -30,6 +35,21 @@ class CountFreqCls:
 		for i in range(0,n):
 			count[self.input[i]] += 1
 		print(count)
+
+	def countFreqEfficient(self):
+		reducedInput = [x-1 for x in self.input]
+		n = len(self.input)
+		for i in range(0,n):
+			reducedInput[reducedInput[i]%n] = reducedInput[reducedInput[i]%n] + n
+		for i in range(0,n):
+			print(i+1,' : ',reducedInput[i]//n)
+		self.countFreqEfficientGetBackOrig(reducedInput)
+
+	def countFreqEfficientGetBackOrig(self,InputModifiedList):
+		n = len(InputModifiedList)
+		for i in range(0,n):
+			InputModifiedList[i] = InputModifiedList[i]%n + 1
+		print(InputModifiedList)
 		
 
 obj = CountFreqCls()
